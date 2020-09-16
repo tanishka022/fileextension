6d510cb568dda2fd98004e077452bd740e20513f
tanishka022 <69908600+tanishka022@users.noreply.github.com>
2020-09-16 23:45:57 +0530

    Created project2.py
    This code will tell the type of file(which the user entered!).

---
+++ project2.py
@@ -0,0 +1,28 @@
+fname = input("Input the Filename: ")
+
+if ".py" in fname:
+    print("The extension of the file is: python")
+
+elif ".txt" in fname:
+    print("The extension of the file is: plain text file")
+
+elif ".bin" in fname:
+    print("The extension of the file is: binary file")
+
+elif ".dat" in fname:
+    print("The extension of the file is: data file")
+
+elif ".db" in fname:
+    print("The extension of the file is: database file")
+
+elif ".sql" in fname:
+    print("The extension of the file is: SQL database file")
+
+elif ".png" in fname:
+    print("The extension of the file is: png image")
+
+elif ".pdf" in fname:
+    print("The extension of the file is: PDF file")
+
+elif ".doc" in fname:
+    print("The extension of the file is: Microsoft Word file")
